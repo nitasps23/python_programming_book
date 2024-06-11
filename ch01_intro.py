e34004f9097aaf2934b4b0ced8eb7156276269d8
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-06-10 23:07:20 -0400

    first commit

---
+++ ch01_intro.py
@@ -0,0 +1,101 @@
+# statement (aka command)
+
+print("Hello, World!")
+
+print("2 + 3 = ", 2+3)
+
+print(2 + 3)
+
+# # function
+# def hello():
+#     print("Hello")
+#     print("Computers are fun!")
+
+# hello()
+
+# parameters (aka arguments)
+
+# def greet(person):
+#     print("Hello", person)
+#     print("How are you?")
+
+# greet("Nita")
+
+print()
+
+# module or script
+# comments
+# variable
+# loop
+# assignment statement
+# butterfly effect
+# names aka identifiers
+
+
+# concat
+print("Bat" + "man")
+print()
+print("Bat", "man", 2, 2 + 3)
+
+print("The answer is", 3 + 4)
+
+# specify ending text
+print("The answer is", end=" ")
+print(3 + 4)
+
+# # assigning input
+# name = input("Enter your name: ")
+# print(name)
+
+# # when user input is number
+# ans = eval(input("Enter an expression: "))
+# print(ans)
+
+
+# simultaneous assignment
+x = 2
+y = 4
+
+sum, diff = x + y, x - y
+
+print(sum, diff)
+
+print()
+
+# swaping values
+temp = x
+x = y
+y = temp
+
+print("x =", x, "y =", y)
+
+print()
+
+x, y = y, x  # simpler swap
+
+print("x =", x, "y =", y)
+
+print()
+
+# for loop
+# counted loop
+# definite loop
+for i in [0, 1, 2, 3]:
+    print(i)
+
+print()
+
+for odd in [1, 3, 5, 7, 9]:
+    print(odd * odd)
+
+print()
+
+for i in range(20):
+    print(i)
+
+print()
+# turn range into list
+print(range(10))
+print(list(range(10)))
+
+
