cc8aa967acd3069f8de08886d9e8daddb5ed2f92
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-06-10 23:04:57 -0400

    first commit

---
+++ ch01_chaos.py
@@ -0,0 +1,10 @@
+# a simple program illustrating chaotic bahavior
+
+def main():
+    print("This program illustrating a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x * (1-x)
+        print(x)
+
+main()
