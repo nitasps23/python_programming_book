68338d040b2f04e53f0c388a8b32a1f45cb4024f
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-06-14 22:26:18 -0400

    first commit

---
+++ ch03_numeric_data.py
@@ -0,0 +1,55 @@
+
+# float vs integer
+print(type(3))
+print(type(3.0))
+
+myInt = -32
+print(type(myInt))
+
+# operations
+print(3 + 4)
+
+print(3.0 + 4.0)
+
+print(3 * 4)
+
+print(3 / 4)
+
+print(3 ** 4)
+
+print(abs(-4))
+
+print(10 / 3)
+
+print(10.0 / 3.0)
+
+print(10 / 5)   # always return a float
+print(10 // 5)  # return int
+
+print(10 % 3) # remainder
+
+# explicit type conversion
+print(int(4.5))
+
+print(int("4"), float("4"))
+
+print(int(3.9))
+
+print(float(4))
+
+print(round(3.9))
+print(round(3.2))
+
+pi = 3.141592653589793
+print(round(pi, 2), round(pi, 3), sep=", ")
+
+
+# math library
+
+# accumulating results: factorials !
+
+print(list(range(10)))
+
+print(list(range(5, 10)))
+
+print(list(range(5, 10, 3)))
