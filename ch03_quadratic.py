aad60932bb48cd25b15fa39a505b2f9184de6a79
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-06-14 22:27:00 -0400

    first commit

---
+++ ch03_quadratic.py
@@ -0,0 +1,22 @@
+# a program that computes the real roots of a quadratic equation
+# illustrates use of the math library
+# note: this program crashes if the equation has no real roots
+
+import math  # make the math library available
+
+def main():
+    print("This program finds the real solutions to a quadratic")
+    print()
+
+    a = float(input("Enter coefficient a: "))
+    b = float(input("Enter coefficient b: "))
+    c = float(input("Enter coefficient c: "))
+
+    discRoot = math.sqrt(b * b - 4 * a * c)
+    root1 = (-b + discRoot) / (2 * a)
+    root2 = (-b - discRoot) / (2 * a)
+
+    print()
+    print("The solutions are:", root1, root2)
+
+main()
