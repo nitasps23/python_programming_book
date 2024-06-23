639333c620e10d0735f5050901157a5e93f63cfe
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-06-22 23:57:07 -0400

    first commit

---
+++ ch05_change2.py
@@ -0,0 +1,15 @@
+# a program to calculate the value of some change in dollars
+# this version represents the total cash in cents
+
+def main():
+    print("Change Counter\n")
+    print("Please enter the count of each coin type.")
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickels = int(input("Nickels: "))
+    pennies = int(input("Pennies: "))
+
+    total = quarters * 25 + dimes * 10 + nickels * 5 + pennies
+    print("\nThe total value of your change is ${0}.{1:0>2}".format(total//100, total%100))
+
+main()
