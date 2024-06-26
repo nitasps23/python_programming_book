60e3d88e4358577c418d5aacc06f607076435c6d
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-06-25 20:55:36 -0400

    first commit

---
+++ ch05_month2.py
@@ -0,0 +1,14 @@
+# a program to print the abbreviation of a month, given its number
+
+def main():
+
+    # month is a list used as a lookup table
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    n = int(input("Enter a month number (1-12): "))
+
+    # print the result
+    print("The month abbreviation is", months[n-1] + ".")
+
+main()
