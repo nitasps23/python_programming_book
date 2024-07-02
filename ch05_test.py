3d1f63e618941e95945f889f6568c75322bbee65
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-07-01 22:33:55 -0400

    first commit

---
+++ ch05_test.py
@@ -0,0 +1,9 @@
+from tkinter.filedialog import askopenfilename
+
+infileName = askopenfilename()
+
+print(infileName)
+
+from tkinter.filedialog import asksaveasfilename
+
+outfileName = asksaveasfilename()
