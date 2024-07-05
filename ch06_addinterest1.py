ca54d151ae8534c2ec32a139c4bb067024b8117a
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-07-05 15:37:23 -0400

    first commit

---
+++ ch06_addinterest1.py
@@ -0,0 +1,14 @@
+
+# not working function
+
+def addInterest(balance, rate):
+    newBalance = balance * (1+rate)
+    balance = newBalance
+
+def test():
+    amount = 1000
+    rate = 0.05
+    addInterest(amount, rate)
+    print(amount)
+
+test()
