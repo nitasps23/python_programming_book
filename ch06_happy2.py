9729147b3ed35c0fcbf93ab48b63545043a054d6
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-07-20 21:31:10 -0400

    first commit

---
+++ ch06_happy2.py
@@ -0,0 +1,16 @@
+def happy():
+    return "Happy Birthday to you!\n"
+
+def verseFor(person):
+    lyrics = happy()*2 + "Happy birthday, dear " + person + ".\n" + happy()
+    return lyrics
+
+def main():
+    for person in ["Fred", "Lucy", "Elmer"]:
+        print(verseFor(person))
+    # outf = open("Happy_Birthday.txt", "w")
+    # for person in ["Fred", "Lucy", "Elmer"]:
+    #     print(verseFor(person), file=outf)
+    # outf.close()
+
+main()
