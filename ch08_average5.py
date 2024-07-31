c9d9a61e016a5d70ba402f589b05065d08a69920
Nita Sokphoodsa <nitasps23@MacBook-Pro.local>
2024-07-30 21:44:01 -0400

    first commit

---
+++ ch08_average5.py
@@ -0,0 +1,16 @@
+
+# file loop
+
+def main():
+    fileName = input("What file are the numbers in? ")
+    infile = open(fileName, 'r')
+    total = 0.0
+    count = 0
+    
+    for line in infile:
+        total = total + float(line)
+        count = count + 1
+    print("\nThe average of the numbers is", total / count)
+
+
+main()
