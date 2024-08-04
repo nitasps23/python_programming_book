d58191bfc38b70acccdce5b34f8b8838abd07f1f
Nita Sokphoodsa <nitasps23@macbook-pro.mynetworksettings.com>
2024-08-03 21:21:43 -0400

    first commit

---
+++ ch08_average6.py
@@ -0,0 +1,17 @@
+
+# end-of-file sentinel loop
+
+def main():
+    fileName = input("What file are the numbers in? ")
+    infile = open(fileName, 'r')
+    total = 0.0
+    count = 0
+    line = infile.readline()
+    while line != "":
+        total = total + float(line)
+        count = count + 1
+        line = infile.readline()
+    print("\nThe average of the numbers is", total / count)
+
+
+main()
