c80c22e6c503285c60fe907ad0b1d7d5cd3fe442
Nita Sokphoodsa <153405782+nitasps23@users.noreply.github.com>
2024-08-18 21:11:41 -0400

    first commit

---
+++ ch10_defining_clases.py
@@ -0,0 +1,33 @@
+
+from graphics import *
+import math
+
+# making circle
+myCircle = Circle(Point(0,0), 20)
+
+print(myCircle) 
+
+
+# defining new classes
+
+# module documentation
+# docstring
+
+import random
+print(random.random.__doc__)
+
+# pydoc
+# help(random.random)
+
+# help(random)
+
+# print(projectile.Projectile.__doc__)
+
+# widgets
+# building button
+
+
+# dialog = InputDialog(45, 40, 2)
+# choice = dialog.interact()
+# if choice == "Fire!":
+#     angle, vel, height = dialog.getValues()
